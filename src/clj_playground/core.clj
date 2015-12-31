e00ac4a82a5dca4ebdc3fe7f60fb5059c7a9c6d8
Andrew Fray <andrew@spryfox.com>
2015-12-31 18:45:56 +0000

    neighbours-of

--- src/clj_playground/core.clj
+++ src/clj_playground/core.clj
@@ -1,6 +1,54 @@
-(ns clj-playground.core)
+(ns clj-playground.core
+  (:require [clojure.set :as set]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn cell [x y]
+  "coordinates of a map cell"
+  {:x x :y y})
+
+(def diagonal-cost (Math/sqrt 2))
+
+(defn traversal [cost cell-coord]
+  {:cell cell-coord :cost cost})
+
+(defn traversal-unit [cell-coord]
+  (traversal 1.0 cell-coord))
+
+(defn traversal-diag [cell-coord]
+  (traversal diagonal-cost cell-coord))
+
+(defn traversal-from-offset [offset]
+  "takes offset as cell"
+  (traversal (Math/sqrt (+ (* (offset :x) (offset :x)) (* (offset :y) (offset :y)))) offset))
+
+(defn translate-traversal [origin translate]
+  (traversal (:cost origin)
+             (cell (+ ((origin :cell) :x) (translate :x))
+                   (+ ((origin :cell) :y) (translate :y)))))
+
+(defn create-grid [width height]
+  "creates grid of set dimensions. width and height are exclusive."
+  {:width width :height height})
+
+(defn valid-cell? [grid c]
+  (and (>= (c :x) 0)
+       (< (c :x) (grid :width))
+       (>= (c :y) 0)
+       (< (c :y) (grid :height))))
+
+(definline validate-cell [grid c]
+  "asserts if any part of the cell isn't in the grid"
+  (assert (>= (c :x) 0))
+  (assert (< (c :x) (grid :width)))
+  (assert (>= (c :y) 0))
+  (assert (< (c :y) (grid :height))))
+
+(defn neighbours-of [grid origin]
+  "4 neighbours around square 2D grid cell. set, because we don't care about order."
+  (validate-cell grid origin)
+  (let [offsets (map traversal-from-offset [(cell  -1 -1) (cell 0 -1) (cell 1 -1)
+                                            (cell -1 0) (cell 1 0)
+                                            (cell -1 1) (cell 0 1) (cell 1 1)])
+        all-neighbours (map #(translate-traversal % origin) offsets)
+        valid-neighbours (filter #(valid-cell? grid (% :cell)) all-neighbours)]
+    (set valid-neighbours)))
+
--- test/clj_playground/core_test.clj
+++ test/clj_playground/core_test.clj
@@ -2,6 +2,41 @@
   (:require [clojure.test :refer :all]
             [clj-playground.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest neighbours-of-t
+  (let [grid-3x3 (create-grid 3 3)
+
+        left-top (cell 0 0)
+        mid-top (cell 1 0)
+        right-top (cell 2 0)
+
+        left-mid (cell 0 1)
+        mid-mid (cell 1 1)
+        right-mid (cell 2 1)
+
+        left-bot (cell 0 2)
+        mid-bot (cell 1 2)
+        right-bot (cell 2 2)]
+    
+    (is (= #{(traversal-diag left-top) (traversal-unit mid-top) (traversal-diag right-top)
+             (traversal-unit left-mid) (traversal-unit right-mid)
+             (traversal-diag left-bot) (traversal-unit mid-bot) (traversal-diag right-bot)}
+           (neighbours-of grid-3x3 mid-mid))
+        "all eight neighbours")
+    (is (= #{(traversal-unit left-top) (traversal-diag mid-top)
+             (traversal-unit mid-mid)
+             (traversal-unit left-bot) (traversal-diag mid-bot)}
+           (neighbours-of grid-3x3 left-mid))
+        "left edge")
+    (is (= #{(traversal-diag mid-top) (traversal-unit right-top)
+             (traversal-unit mid-mid)
+             (traversal-diag mid-bot) (traversal-unit right-bot)}
+           (neighbours-of grid-3x3 right-mid))
+        "right edge")
+    (is (= #{(traversal-unit left-top) (traversal-unit right-top)
+             (traversal-diag left-mid) (traversal-unit mid-mid) (traversal-diag right-mid)}
+           (neighbours-of grid-3x3 mid-top))
+        "top edge")
+    (is (= #{(traversal-diag left-mid) (traversal-unit mid-mid) (traversal-diag right-mid)
+             (traversal-unit left-bot) (traversal-unit right-bot)}
+           (neighbours-of grid-3x3 mid-bot))
+        "bottom edge")))
