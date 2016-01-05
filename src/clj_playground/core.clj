059edebcc1100a798a04b24de39b0625a15e6a7c
Andrew Fray <andrew@spryfox.com>
2016-01-05 13:05:12 +0000

    more idiomatic clojure across the board

--- src/clj_playground/core.clj
+++ src/clj_playground/core.clj
@@ -12,12 +12,12 @@
   {:cell cell-coord :cost cost})
 
 (defn traversal-cost [from-cell to-cell]
-  (let [offset (cell (- (to-cell :x) (from-cell :x)) (- (to-cell :y) (from-cell :y)))]
-    (Math/sqrt (+ (* (offset :x) (offset :x)) (* (offset :y) (offset :y))))))
+  (let [offset (cell (- (:x to-cell) (:x from-cell)) (- (:y to-cell) (:y from-cell)))]
+    (Math/sqrt (+ (* (:x offset) (:x offset)) (* (:y offset) (:y offset))))))
 
-(defn translate-cell [origin translate]
-  (cell (+ (origin :x) (translate :x))
-        (+ (origin :y) (translate :y))))
+(defn translate-cell [translate origin]
+  (cell (+ (:x origin) (:x translate))
+        (+ (:y origin) (:y translate))))
 
 (defn create-grid  
   "creates grid of set dimensions. width and height are exclusive."
@@ -26,10 +26,8 @@
 
 (defn valid-cell? [grid c]
   {:pre [(not-any? nil? [grid c])]}
-  (and (>= (c :x) 0)
-       (< (c :x) (grid :width))
-       (>= (c :y) 0)
-       (< (c :y) (grid :height))))
+  (and (< -1 (:x c) (:width grid))
+       (< -1 (:y c) (:height grid))))
 
 (defn neighbours-of 
   "neighbours around square 2D grid cell. returns {:neighbours :traversals} 
@@ -40,8 +38,8 @@
   (let [valid-neighbours (->> [(cell  -1 -1) (cell 0 -1) (cell 1 -1)
                                (cell -1 0) (cell 1 0)
                                (cell -1 1) (cell 0 1) (cell 1 1)]
-                              (map #(translate-cell % origin))
-                              (filter #(valid-cell? grid %)))]
+                              (map (partial translate-cell origin))
+                              (filter (partial valid-cell? grid)))]
     {:neighbours (set valid-neighbours)
      :traversals (reduce #(assoc %1 %2 (traversal-cost %2 origin)) {} valid-neighbours)}))
 
@@ -54,11 +52,11 @@
    :post [(some? %)]}
   (loop [current-cell current-cell
          created-route '()]
-    (let [parent (parents current-cell)]
-      (if (nil? parent) ;; at end of the road
-        created-route 
-        (recur parent
-               (conj created-route current-cell))))))
+    (if-let [parent (parents current-cell)]
+      (recur parent
+             (conj created-route current-cell))
+      ;; else parent is nil, and it's the end of the road
+      created-route))) 
 
 (defn navigate-to 
   "provides route from start to dest, not including start"
@@ -87,7 +85,7 @@
                                       (and (contains? unvisited-neighbours open-cell)
                                            (> current-cost
                                               (neighbour-total-distances open-cell)))))
-                                   (map #(% :cell))
+                                   (map :cell)
                                    set)
             updated-open (map (fn [{open-cell :cell current-cost :cost :as open-node}]
                                (if (contains? better-neighbours open-cell)
@@ -96,7 +94,7 @@
                               open-nodes)
             updated-parents (reduce #(assoc %1 %2 current-cell) {} better-neighbours)
             ;; ...then create new neighbours
-            all-open-cells (set (map #(% :cell) open-nodes))
+            all-open-cells (set (map :cell open-nodes))
             new-neighbours (set/difference unvisited-neighbours all-open-cells)
             new-open-nodes (map #(traversal (neighbour-total-distances %) %) new-neighbours)
             new-parents (reduce #(assoc %1 %2 current-cell) {} new-neighbours)]
@@ -104,5 +102,5 @@
                (conj visited-cells current-cell)
                (merge parents updated-parents new-parents))))))
 
-;;(navigate-to (create-grid 5 5) (cell 0 0) (cell 0 -2))
+;;(navigate-to (create-grid 5 5) (cell 0 0) (cell 0 4))
 
